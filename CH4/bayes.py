3c13ad28daa17667165148600210d6cb6cb1ac0c
wangc <20123200044@m.scnu.edu.cn>
2018-04-03 22:09:03 +0800

    朴素贝叶斯分类器的初步实现及测试

--- CH4/bayes.py
+++ CH4/bayes.py
@@ -35,19 +35,42 @@ def setOfWords2Vec(vocabList, inputSet):
 def trainNB0(trainMatrix, trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
-    pAbusive = sum(trainCategory)/float(numTrainDocs)
-    p0Num = zeros(numWords); p1Num = zeros(numWords)
-    p0Denom = 0.0; p1Denom = 0.0
+    pAbusive = sum(trainCategory)/float(numTrainDocs) #训练集中“粗鲁”类型文件的比例 
+    p0Num = ones(numWords); p1Num = ones(numWords)
+    p0Denom = 2.0; p1Denom = 2.0
     for i in range(numTrainDocs):
         if trainCategory[i] == 1:
-            p1Num += trainMatrix[i]
-            p1Denom += sum(trainMatrix[i])
+            p1Num += trainMatrix[i]         #p1Num每一行的每一元素为该单词在“粗鲁”类别中出现的总次数
+            p1Denom += sum(trainMatrix[i])  #粗鲁文件的所有单词总数
         else:
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
-        p1Vect = p1Num/p1Denom  #change to log()
-        p0Vect = p0Num/p0Denom  #change to log()
+        p1Vect = log(p1Num/p1Denom)  #字典中的每个单词在标记为“粗鲁”的文件中出现的比例
+        p0Vect = log(p0Num/p0Denom)  #字典中的每个单词在标记为“非粗鲁”的文件中出现的比例
         return p0Vect,p1Vect,pAbusive
+
+#
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = sum(vec2Classify * p1Vec) + log(pClass1)
+    p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+def testingNB():
+    listOPosts,listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
+    p0V, p1V,pAb = trainNB0(array(trainMat),array(listClasses))
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry, 'classfied as: ', classifyNB(thisDoc, p0V, p1V,pAb))
+    testEntry = ['stupid','garbage']
+    thisDoc   = array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry, 'classfied as: ', classifyNB(thisDoc, p0V, p1V,pAb))
 
 
 
